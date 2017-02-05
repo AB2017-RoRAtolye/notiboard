a9165d1c02dc5dbf4b71c214cd9948339fbce426
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2017-02-05 16:36:38 +0300

    Add default image to board logo

--- app/uploaders/board_logo_uploader.rb
+++ app/uploaders/board_logo_uploader.rb
@@ -15,12 +15,9 @@ class BoardLogoUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    ActionController::Base.helpers.asset_path("placeholder.png")
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
