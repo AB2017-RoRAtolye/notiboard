6bce2512723d4deb93a8279f63f76743eaac6d2b
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2017-02-05 11:40:54 +0300

    Add Boards#show page

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,6 @@
 class BoardsController < ApplicationController
   def show
+    @board = Board.find(params[:id])
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root to: "boards#index"
+
   get "boards", to: "boards#index"
   get "boards/new", to: "boards#new"
   post "boards", to: "boards#create"
+  get "boards/:id", to: "boards#show", as: :board
 end
