ecf7b50412020b10395746a21aa36a8ae7061a97
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2017-02-07 10:29:03 +0300

    Add post new form, add post list to board page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,19 +3,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @board = Board.find(params[:board_id])
     @post = Post.new
   end
 
   def create
-    # Create post connected to Board and current user
+    @board = Board.find(params[:board_id])
+    @post = @board.posts.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to board_path(@board), notice: "Duyuru olusturuldu"
+    else
+      render :new
+    end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image, :remove_image)
   end
 end
